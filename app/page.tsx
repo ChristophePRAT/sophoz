b42f182b04c3b546b0b0ef190b3d3be7116858eb
Christophe Prat <christopheprat@icloud.com>
2023-04-29 01:23:37 +0200

    idk what this commit is for but latest version ibizaaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,124 @@
-import Home from './home'
-import { Metadata } from 'next'
+"use client"
+import Image from 'next/image'
+import styles from './page.module.scss'
+import { Playfair_Display, Inter } from "@next/font/google"
+import { ChevronDown, Zap } from "react-feather"
+import dynamic from 'next/dynamic'
+import { useRef, useEffect, useState, useMemo } from 'react'
+import articleContent from '../src/article'
+import Demo from "../public/demo.png"
 
-export const metadata: Metadata = {
-  title: 'Home',
-  description: 'Home page for the Sophoz website',
-  keywords: ['sophoz', 'news', 'artificial intelligence', 'machine learning', 'technology', 'politics'],
+const Logo = dynamic(() => import('./logo'), {
+  ssr: false,
+})
+
+// Import IconsGathering dynamically
+const IconsGathering = dynamic(() => import('./iconsGathering'), {
+  ssr: false,
+})
+
+const garamond = Playfair_Display({
+  variable: "--serif-font",
+  subsets: ["latin"],
+})
+const inter = Inter({
+  variable: '--sans-font',
+  subsets: ["latin"],
+})
+
+// Create an string extension to shuffle the words
+const shuffle = (str: string) => {
+  var a = str.split(" ")
+  a.sort(() => Math.random() - 0.5)
+  return a.join(" ")
 }
-export default Home
+const mainStyles = [styles.main, garamond.variable, inter.variable].join(' ')
+
+export default function Home() {
+  const firstSectionRef = useRef(null)
+  const secondSectionRef = useRef(null)
+  const [text, setText] = useState(articleContent)
+  const [circleRadius, setCircleRadius] = useState(0)
+
+  const handleScroll = () => {
+    const element: any = secondSectionRef.current
+    if (element) {
+      const secondPercentage = element.offsetTop / (2 * element.offsetHeight)
+
+      if (secondPercentage === 0) {
+        if (Math.random() > 0.5) {
+          setText((text) => shuffle(text))
+        }
+
+      } else {
+        setCircleRadius(secondPercentage)
+      }
+    }
+  }
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <main className={mainStyles}>
+      <section className={styles.topSection}>
+        <div className={styles.noise}></div>
+        <div className={styles.group}>
+          <h2 className={styles.subtitle}>Introducing</h2>
+          <h1 className={styles.title}>Sophoz</h1>
+          <h2 className={styles.heading}>Ordinary news for<br/> extraordinary people</h2>
+        </div>
+        <div className={styles.group}>
+          {/* This will be the most beautiful button ever designed*/}
+          <a className={styles.waitlist} href="https://tally.so/r/mDBjbZ">
+            Join waitlist
+            <Zap fill="black" />
+          </a>
+          <ChevronDown className={styles.chevron} size={48} />
+        </div>
+      </section>
+      <div className={styles.scrollSection}>
+        <section className={styles.hardToReadSection} ref={firstSectionRef}>
+          <div className={styles.left}>
+            <h2 className={styles.heading}>Hard to read</h2>
+            <p className={styles.paragraph}>
+              News can be overwhelming. Dozens of paragraphs to say nothing. It should not be this hard to get informed.
+
+            </p>
+          </div>
+          <div className={styles.right}>
+            <p className={styles.backgroudText}>{text}</p>
+          </div>
+        </section>
+      </div>
+      <div className={styles.scrollSection}>
+        <section className={styles.tooSparseSection} ref={secondSectionRef}>
+          <div className={styles.right}>
+            <h2 className={styles.heading}>Too sparse</h2>
+            <p className={styles.paragraph}>
+              Information is everywhere. And so is misinformation.<br /> You deserve better news. Using AI, we will clear out the noise.
+            </p>
+          </div>
+          <div className={styles.left}>
+            <IconsGathering percentage={circleRadius} />
+          </div>
+
+        </section>
+      </div>
+      <section className={styles.centered}>
+        <h2 className={styles.heading}>
+          Understand the world
+        </h2>
+        <p className={styles.paragraph}>With a detailed and structured summary of a topic, get a better comprehension of the arguments of the different sides. Whether you are an expert or a begginner, the summary will provide you the useful information to know what is happening on your phone.</p>
+          <Image className={styles.demo} src={Demo} alt="iPhone 14 Pro with Sophoz app" />
+      </section>
+      <footer className={styles.footer}>
+        <p>© 2023 Sophoz</p>
+      </footer>
+    </main>
+  )
+}
+
