9e5ddb3a95c636a2521234b565451bc2d5809d32
ChristophePRAT <christopheprat@icloud.com>
2023-07-24 00:29:46 +0200

    Added Google login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,28 @@
 // @ts-nocheck
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
+import { createServerActionClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { revalidatePath } from 'next/cache'
 import { cookies } from 'next/headers'
 import { redirect,  } from 'next/navigation'
 import SubmitButton from "./SubmitButton"
+import Google from "/public/google.svg"
+import Image from "next/image"
+
 import type { Database } from '@/types/supabase'
 
 export default async function Login() {
+  const supabase = createServerComponentClient<Database>({ cookies })
+  const { data: { session } } = await supabase.auth.getSession()
+  if (session && session.user) {
+    redirect("/balanced_news")
+  }
+
+
   const handleSendLink = async (formData: FormData) => {
     "use server";
     const email = String(formData.get('email'))
 
     const supabase = createServerActionClient<Database>({ cookies })
-    const { data: { session } } = await supabase.auth.getSession()
+    //const { data: { session } } = await supabase.auth.getSession()
     const baseURL = process.env.VERCEL_ENV == "production" ? "https://sophoz.app" : "http://localhost:3000"
     const redirectURL = `${baseURL}/api/auth/callback`
     console.log(redirectURL)
@@ -28,15 +38,45 @@ export default async function Login() {
     }
     revalidatePath("/login")
   }
+  const handleGoogleSignIn = async () => {
+    "use server";
+    console.log("fasopidfjaiposdjf")
+    const supabase = createServerActionClient<Database>({ cookies })
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        queryParams: {
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+
+      },
+    })
+
+    if (error) {
+      console.log(error)
+      return
+    }
+    if (data && data.url) {
+      redirect(data.url)
+    }
+    revalidatePath("/login")
+  }
 
   return (
     <div className="flex flex-col justify-center items-center m-auto h-screen w-screen">
       <h1 className="text-6xl font-bold text-center my-4">Login</h1>
       <h2 className="text-2xl text-center">Easily login using a magic link</h2>
       <form className="flex flex-col justify-center p-4 items-center" action={handleSendLink}>
-        <input type="email" placeholder="Enter email" className="p-2 rounded-xl m-2 text-md outline-none bg-gray-200 text-black w-96" name="email" />
-        <SubmitButton />
+        <button className="p-2 rounded-xl m-2 text-md outline-none bg-gray-200 text-black w-96 flex justify-center items-center hover:bg-gray-300  transition transition-all" formAction={handleGoogleSignIn}><Image src={Google} alt="Google" width={24} height={24} className="mr-2" />Login with Google</button>
+        <p className="text-sm py-4 text-center text-gray-500 font-bold tracking-tight">OR</p>
+        <div className="flex flex-row">
+          <input type="email" placeholder="Enter email" className="p-2 rounded-xl m-2 text-md outline-none bg-gray-200 text-black w-96" name="email" />
+          <SubmitButton />
+        </div>
       </form>
+      <footer className="absolute bottom-0 left-0 right-0 h-16 bg-gray-200 flex justify-center items-center"><a target="_blank" href="https://icons8.com/icon/60984/google">Google</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></footer>
     </div>
   )
 }
