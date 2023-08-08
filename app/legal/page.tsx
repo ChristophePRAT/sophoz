a3a681c4e58db51cec0519ab5250914d6706f06d
ChristophePRAT <christopheprat@icloud.com>
2023-08-08 23:08:34 +0100

    Add legal

---
+++ app/legal/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Policies() {
+	return (
+		<div>
+			<h1>Policies</h1>
+			<p>None of your data is used. We keep your email address for the login but it is not linked to the queries you make.</p>
+		</div>
+	)
+}
